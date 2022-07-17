8930953d8e715a5b3d1520caf037984ac29d8a13
Olivier Roos <roosolivier@gmail.com>
2022-07-17 17:31:38 +0200

    add popup for project download

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleDownloadAmbush(){
+    let body = document.getElementById("body")
+    body.classList.toggle("blur")
+    let ambushPopup = document.getElementById("ambushPopup")
+    ambushPopup.classList.toggle("hidden")
+}
